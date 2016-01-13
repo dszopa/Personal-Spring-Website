cf640f1876bdf1d66d3345aa3125c5f3da900da3
Daniel Szopa <szopanator@gmail.com>
2016-01-12 21:00:44 -0600

    Update Project

--- src/main/java/me/dszopa/website/Application.java
+++ src/main/java/me/dszopa/website/Application.java
@@ -3,17 +3,16 @@ package me.dszopa.website;
 import me.dszopa.website.config.ControllerConfig;
 import me.dszopa.website.config.RepoConfig;
 import me.dszopa.website.config.ServiceConfig;
+import me.dszopa.website.config.WebSecurityConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by danny on 12/23/15.
  */
-@Configuration
 @EnableAutoConfiguration
 @ComponentScan
 public class Application {
@@ -22,6 +21,6 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(new Object[] {Application.class, RepoConfig.class, ServiceConfig.class,
-                ControllerConfig.class}, args);
+                ControllerConfig.class, WebSecurityConfig.class}, args);
     }
 }
--- src/main/java/me/dszopa/website/config/ControllerConfig.java
+++ src/main/java/me/dszopa/website/config/ControllerConfig.java
@@ -5,7 +5,6 @@ import me.dszopa.website.service.interfaces.ProgrammingIdeaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
